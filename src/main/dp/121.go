1f0e2cb8aa90a2e51f5d0f36501deb5f3477a0c4
leidongxing <leidongxing@gmail.com>
2020-05-18 23:53:12 +0800

    dp 121

---
+++ src/main/dp/121.go
@@ -0,0 +1,14 @@
+package dp
+
+func maxProfit(prices []int) int {
+	var max int = 0
+	var paid int = 1008611
+	for i := 0; i < len(prices); i++ {
+		if paid > prices[i] {
+			paid = prices[i]
+		} else if max < prices[i]-paid {
+			max = prices[i] - paid
+		}
+	}
+	return max
+}
