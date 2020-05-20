75e5129b8e48574c81d4cebe9f3db642604c4526
leidongxing <leidongxing@gmail.com>
2020-05-21 00:31:34 +0800

    dp 120

---
+++ src/main/dp/120.go
@@ -0,0 +1,28 @@
+package dp
+
+import "testing"
+
+func minimumTotal(triangle [][]int) int {
+	if len(triangle) == 0 {
+		return 0
+	}
+	dp := triangle[len(triangle)-1]
+	for i := len(triangle) - 2; i >= 0; i-- {
+		for j := 0; j <= i; j++ {
+			dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
+		}
+	}
+	return dp[0]
+}
+
+func min(a int, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func Test(t *testing.T) {
+	var triangle = [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
+	minimumTotal(triangle)
+}
