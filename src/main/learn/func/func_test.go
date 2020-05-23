679b0fc7b9eb7a8df8e9cdf7f0e4a871fb2f02b2
leidongxing <leidongxing@gmail.com>
2020-05-24 01:05:03 +0800

    func

---
+++ src/main/learn/func/func_test.go
@@ -0,0 +1,34 @@
+package _func
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+//函数可以返回多个返回值
+func returnMultiValues() (int, int) {
+	return rand.Intn(20), rand.Intn(20)
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+		fmt.Print("time spend:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFun(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	a, _ := returnMultiValues()
+	t.Log(a)
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10))
+}
