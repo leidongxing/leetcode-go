123d3c3668c87720b5559f1d210be8c69b4f1f60
leidongxing <leidongxing@gmail.com>
2020-05-24 14:13:37 +0800

    defer 延迟执行

--- src/main/learn/func/func_test.go
+++ src/main/learn/func/func_test.go
@@ -32,3 +32,30 @@ func TestFn(t *testing.T) {
 	tsSF := timeSpent(slowFun)
 	t.Log(tsSF(10))
 }
+
+//可变长参数
+func Sum(ops ...int) int {
+	ret := 0
+	for _, op := range ops {
+		ret += op
+	}
+	return ret
+}
+
+func TestVarParam(t *testing.T) {
+	t.Log(Sum(1, 2, 3, 4))
+	t.Log(Sum(1, 2, 3, 4, 5))
+}
+
+//延迟执行  实现类似于try catch finally释放资源的效果
+func TestDefer(t *testing.T) {
+	defer Clear()
+	fmt.Println("start")
+	//退出程序
+	panic("error")
+	fmt.Println("end")
+}
+
+func Clear() {
+	fmt.Println("Clear resources.")
+}
