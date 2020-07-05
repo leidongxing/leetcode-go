00880b7d30aa253124ece66910f90b44fb17696d
leidongxing <leidongxing@gmail.com>
2020-07-05 23:56:40 +0800

    share mem

---
+++ src/learn/lock/share_mem_test.go
@@ -0,0 +1,38 @@
+package lock
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCounter(t *testing.T) {
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	//强制休眠1s
+	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
+
+func TestCounterSafe(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("counter = %d", counter)
+}
