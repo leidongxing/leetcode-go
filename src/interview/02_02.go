505b73ec9580ad95b676c295632cd6679f84258b
leidongxing <leidongxing@gmail.com>
2020-07-12 23:53:30 +0800

    返回倒数第 k 个节点

---
+++ src/interview/02_02.go
@@ -0,0 +1,14 @@
+package interview
+
+//返回倒数第 k 个节点
+func kthToLast(head *ListNode, k int) int {
+	slow, fast := head, head
+	for i := 0; i < k; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow.Val
+}
