ec96ecb8ba39269d64fa6eca6e1a76c223e580ea
leidongxing <leidongxing@gmail.com>
2020-08-07 00:23:28 +0800

    相同的树

---
+++ src/tree/100_test.go
@@ -0,0 +1,11 @@
+package tree
+
+//相同的树
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
